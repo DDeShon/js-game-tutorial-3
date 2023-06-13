5cd8ccc5f7496126916f8790f447e764870e9378
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-06-13 13:38:21 -0500

    working on enemy class

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 800;
 
-enemy1 = {
-  x: 0,
-  y: 0,
-  width: 200,
-  height: 200,
-};
+class Enemy {
+  constructor() {
+    (this.x = 10), (this.y = 50), (this.width = 100), (this.height = 100);
+  }
+}
+
+const enemy1 = new Enemy();
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
